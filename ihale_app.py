94970af8ecfa8598e79fab9bb95bf05e59e09e3e
mhmtkt <34mk61mk@gmail.com>
2025-06-23 18:38:13 +0300

    Update ihale_app.py

--- ihale_app.py
+++ ihale_app.py
@@ -8,7 +8,26 @@ from firebase_admin import credentials, firestore
 
 # Firebase Admin SDK'yı başlat
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_service_account.json")
+ import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+
+# Secrets içinden firebase configini oku
+firebase_config = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+}
+
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred)
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
